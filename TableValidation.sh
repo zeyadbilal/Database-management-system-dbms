3a1b642a47fc9b3e54cb8337b7b6841d3f41a9af
Zeyad Bilal <zeyad4bilal@gmail.com>
2026-04-25 14:45:11 +0300

    Add files via upload

--- TableValidation.sh
+++ TableValidation.sh
@@ -1,49 +1,30 @@
 #!/bin/bash
 valid=0
-if [[ $tablename=[a-zA-Z]*([a-z] | [_-] | [0-9] | [A-Z] ) ]];
+if [[ $tablename=[a-zA-Z]*([A-Z]|[_-]|[a-z]|[0-9]) ]];
 then
-	if [[ $1 -eq 0 ]]; #for create table 0
-	then
-		if ! [[ -e ~/DB/$dbname/$tablename ]];
-		then
-			valid=1
-		else
-			echo "╔══════════════════════════════════════════════════════════╗"
-			echo "║                    ❗ ERROR MESSAGE                      ║"
-			echo "╠══════════════════════════════════════════════════════════╣"
-			echo "║                                                          ║"
-			echo "║    Table [$tablename] is already exists!                 ║"
-			echo "║    Please choose a different name.                       ║"
-			echo "║                                                          ║"
-			echo "╚══════════════════════════════════════════════════════════╝"
-		fi
-	elif [[ $1 -eq 1 ]]; #for all
-	then
-		if ! [[ -e ~/DB/$dbname/$tablename ]];
-		then
-			echo "╔══════════════════════════════════════════════════════════╗"
-			echo "║                    ❗ ERROR MESSAGE                      ║"
-			echo "╠══════════════════════════════════════════════════════════╣"
-			echo "║                                                          ║"
-			echo "║    Table [$tablename] is not exist!                      ║"
-			echo "║    Please check the table name and try again.            ║"
-			echo "║                                                          ║"
-			echo "╚══════════════════════════════════════════════════════════╝"
-
-		
-		else
-			valid=1
-		fi
-	fi
-else 
-	echo "╔══════════════════════════════════════════════════════════╗"
-	echo "║                    ⚠ WARNING MESSAGE                     ║"
-	echo "╠══════════════════════════════════════════════════════════╣"
-	echo "║                                                          ║"
-	echo "║    The table name must not contain special characters.   ║"
-	echo "║    Please use only letters, numbers, and underscores.    ║"
-	echo "║                                                          ║"
-	echo "╚══════════════════════════════════════════════════════════╝"
+    if [[ $1 -eq 0 ]]; # 0 for create table
+    then
+        if ! [[ -e ~/DB/$dbname/$tablename ]];
+        then
+            valid=1
+        else
+            echo "----------------------------------------------------------------------"
+            echo "-----------------Table [$tablename] is alredy exist------------------"
+            echo "----------------------------------------------------------------------"
+        fi
+    elif [[ $1 -eq 1 ]]; # for all
+    then
+        if ! [[ -e ~/DB/$dbname/$tablename ]];
+        then
+            echo "----------------------------------------------------------------------"
+            echo "--------------------Table [$tablename] is not exist-----------------"
+            echo "----------------------------------------------------------------------"
+        else
+            valid=1
+        fi
+    fi
+else
+    echo "----------------------------------------------------------------------"
+    echo "-----------The Table name not contain spechial characters-------------"
+    echo "----------------------------------------------------------------------"
 fi
-
-		

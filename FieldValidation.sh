d94d434ab2e0b1a0b6b4f80556b8962573b0824c
Zeyad Bilal <zeyad4bilal@gmail.com>
2026-04-25 14:36:31 +0300

    Add files via upload

--- FieldValidation.sh
+++ FieldValidation.sh
@@ -1,50 +1,30 @@
 #!/bin/bash
 valid=0
-if [[ $fieldname=[a-zA-Z]*([a-z] | [A-Z] | [0-9] | [_-] ) ]];
+if [[ $fieldname = [A-Za-z]*([A-Z]|[_-]|[a-z]|[0-9]) ]];
 then
-	if [[ $1 -eq 0 ]]; #for create field
-	then
-		if ! [[ $(cut -d":" -f 1 $tablename".meta" | grep -w "^$fieldname$") ]];
-		then
-			valid=1
-		else
-			echo "╔══════════════════════════════════════════════════════════╗"
-			echo "║                    ❗ ERROR MESSAGE                      ║"
-			echo "╠══════════════════════════════════════════════════════════╣"
-			echo "║                                                          ║"
-			echo "║    Field [$fieldname] is already exist!                  ║"
-			echo "║    Please choose a different field name.                 ║"
-			echo "║                                                          ║"
-			echo "╚══════════════════════════════════════════════════════════╝"
-
-		fi
-	elif [[ $1 -eq 1 ]]; #for all
-	then
-		if ! [[ $(cut -d":" -f 1 $tablename".meta" | grep -w "^$fieldname$") ]]; # echo $? for condition
-		then
-			echo "╔══════════════════════════════════════════════════════════╗"
-			echo "║                    ❗ ERROR MESSAGE                      ║"
-			echo "╠══════════════════════════════════════════════════════════╣"
-			echo "║                                                          ║"
-			echo "║    Field [$fieldname] is not exist!                      ║"
-			echo "║    Please check the field name and try again.            ║"
-			echo "║                                                          ║"
-			echo "╚══════════════════════════════════════════════════════════╝"
-		else
-			valid=1
-		fi
-	fi
+    if [[ $1 -eq 0 ]]; # 0 for create field
+    then
+        if ! [[ $(cut -d":" -f 1 $tablename".meta" | grep -w "^$fieldname$") ]];
+        then
+            valid=1
+        else
+            echo "----------------------------------------------------------------------"
+            echo "------------------Field [$fieldname] is alredy exist------------------"
+            echo "----------------------------------------------------------------------"
+        fi
+    elif [[ $1 -eq 1 ]]; # for all
+    then
+        if ! [[ $(cut -d":" -f 1 $tablename".meta" | grep -w "^$fieldname$") ]]; # echo $? for condition
+        then
+            echo "----------------------------------------------------------------------"
+            echo "--------------------Field [$fieldname] is not exist-------------------"
+            echo "----------------------------------------------------------------------"
+        else
+            valid=1
+        fi
+    fi
 else
-	echo "╔══════════════════════════════════════════════════════════╗"
-	echo "║                    ⚠ WARNING MESSAGE                     ║"
-	echo "╠══════════════════════════════════════════════════════════╣"
-	echo "║                                                          ║"
-	echo "║    The database name must not contain special characters.║"
-	echo "║    Please use only letters, numbers, and underscores.    ║"
-	echo "║                                                          ║"
-	echo "╚══════════════════════════════════════════════════════════╝"
+    echo "----------------------------------------------------------------------"
+    echo "-------- The database name not contain spechial characters -----------"
+    echo "----------------------------------------------------------------------"
 fi
-
-
-
-
--- SelectFromTable.sh
+++ SelectFromTable.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 while true
 do
-	read -p "[$dbname] Select From <table name>: "tablename
-	source TableValidation 1
-	if [[ $valid -eq 1 ]]
-	then
-		break
-	fi
+    read -p "[$dbname] Select From <table name>: " tablename
+    source TableValidation.sh 1
+    if [[ $valid -eq 1 ]]
+    then 
+        break
+    fi
 done
-source SelectMenu.sh
 
+source SelectMenu.sh
